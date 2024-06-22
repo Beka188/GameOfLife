83df0d40bd6cf1238105fca98ee77cfca67b86f5
Beka188 <esil.seitkalyk@gmail.com>
2024-06-22 12:24:01 +0500

    Basic implementation of flags

--- main.go
+++ main.go
@@ -2,16 +2,31 @@ package main
 
 import (
 	"crunch03/models"
+	"crunch03/utils"
 	"fmt"
 	"github.com/eiannone/keyboard"
 	"log"
 )
 
 func main() {
+	arr, e := utils.ReadFlags()
+	if e != nil {
+		log.Fatal(e)
+	}
+	if arr[2] == 1 {
+		return
+	}
 	m, err := models.NewMatrix()
 	if err != nil {
 		panic(err)
 	} else {
+		if arr[0] > 0 {
+			m.IsVerbose = true
+		}
+		if arr[1] > 0 {
+			m.IsVerbose = true
+			m.DelayMs = arr[1]
+		}
 		printMatrix(*m)
 	}
 	if err := keyboard.Open(); err != nil {
@@ -35,7 +50,7 @@ func main() {
 }
 
 func printMatrix(m models.Matrix) {
-	fmt.Printf("%d\n\n", m.TickCount)
+	fmt.Printf("%d   %d  \n\n", m.TickCount, m.DelayMs)
 	for _, row := range m.Body {
 		for j, cell := range row {
 			if cell.Live {
--- utils/flags.go
+++ utils/flags.go
@@ -1,11 +1,36 @@
 package utils
 
-import "os"
+import (
+	"os"
+	"strconv"
+)
 import "fmt"
 
-func ReadFlags() []string {
-	return os.Args[1:]
-
+func ReadFlags() ([3]int, error) {
+	flags := [3]int{-1, -1, -1}
+	args := os.Args[1:]
+	if len(args) == 0 {
+		return [3]int{-1, -1, -1}, nil
+	} else {
+		for i, arg := range args {
+			if arg == "--help" && i == 0 {
+				fmt.Println("Usage: go run main.go [options]\n\nOptions:\n  --help        : Show the help message and exit\n  --verbose     : Display detailed information about the simulation, including grid size, number of ticks, speed, and map name\n  --delay-ms=X: Set the animation speed in milliseconds. Default is 2500 milliseconds")
+				return [3]int{-1, -1, 1}, nil
+			} else if arg == "--verbose" {
+				flags[0] = 1
+			} else if len(arg) > 10 && arg[:11] == "--delay-ms=" {
+				ms, err := strconv.Atoi(arg[11:])
+				if err == nil {
+					flags[1] = ms
+				} else {
+					return [3]int{-1, -1, -1}, fmt.Errorf("invalid value for --delay-ms flag: %d", ms)
+				}
+			} else {
+				return [3]int{-1, -1, -1}, fmt.Errorf("no correct flags provided. Use --help for more information")
+			}
+		}
+	}
+	return flags, nil
 }
 
 func flagHelp() {
@@ -17,5 +42,4 @@ func flagVerbose() {
 }
 
 func flagDelayMs() {
-	fmt.Println("Usage: go run main.go [options]\n\nOptions:\n  --help        : Show the help message and exit\n  --verbose     : Display detailed information about the simulation, including grid size, number of ticks, speed, and map name\n  --delay-ms=X: Set the animation speed in milliseconds. Default is 2500 milliseconds")
 }
